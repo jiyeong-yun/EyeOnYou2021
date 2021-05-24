15f70e1b50d63856bbc44a1b31f3cd21ce04c33f
yun-jiyeong <70522500+jiyeong-yun@users.noreply.github.com>
2021-05-25 00:16:04 +0900

    InfoActivity 생성

---
+++ tensorflow/tools/android/test/src/org/tensorflow/demo/InfoActivity.java
@@ -0,0 +1,71 @@
+package org.tensorflow.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class InfoActivity extends Activity {
+    public TextToSpeech tts;
+    String speechtext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        //TODO: 실행하자마자 음성 출력
+        tts = new TextToSpeech(InfoActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status == TextToSpeech.SUCCESS) {
+                    // 언어를 선택한다.
+                    int result = tts.setLanguage(Locale.KOREA);
+                    //if (result==TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED)
+                    // {
+                    //     Toast.makeText(activity_weather.this, "인식 버튼 클릭", Toast.LENGTH_SHORT).show();
+                    // }
+                }
+            }
+        });
+
+        tts.setPitch(1.0f);         // 음성 톤을 1.0배 올려준다.
+        tts.setSpeechRate(1.0f);    // 읽는 속도는 기본 설정
+        speechtext="이 어플리케이션은 카메라를 통해 보여진 옷의 무늬 또는 색을 알려줍니다. " +
+                "다음 화면의 오른쪽 하단 버튼을 누르면 옷의 정보를 알려줍니다. " +
+                "왼쪽 하단 버튼을 누르면 날씨 정보를 알려줍니다.";
+        tts.speak(speechtext, TextToSpeech.QUEUE_FLUSH, null);
+
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                //손가락으로 화면을 누르기 시작했을 때 할 일
+                //인식 화면으로 넘어감
+                Intent intent = new Intent(getApplicationContext(), DetectorActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                //터치 후 손가락을 움직일 때 할 일
+                break;
+            case MotionEvent.ACTION_UP:
+                //손가락을 화면에서 뗄 때 할 일
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                // 터치가 취소될 때 할 일
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+}
