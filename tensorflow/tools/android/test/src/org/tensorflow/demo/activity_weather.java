692ec9907865fd375bd1d4e4fae3379eb5e47162
yun-jiyeong <70522500+jiyeong-yun@users.noreply.github.com>
2021-05-27 00:54:49 +0900

    실수로 지운 tts 살림

--- tensorflow/tools/android/test/src/org/tensorflow/demo/activity_weather.java
+++ tensorflow/tools/android/test/src/org/tensorflow/demo/activity_weather.java
@@ -272,6 +272,7 @@ public class activity_weather extends Activity {
                         "오늘 날씨에는 " + clothesup +"을 추천 드립니다. "
                         +name +"옷과 어울리는 "+color+" 하의를 추천 드립니다. " +
                         "옷을 재인식하고 싶으시면 하단의 버튼을 눌러주세요. ";
+                tts.speak(speechtext, TextToSpeech.QUEUE_FLUSH, null);
 
 
 
