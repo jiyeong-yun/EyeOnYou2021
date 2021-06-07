f70280de27ce50307f50f68fdee3b485aab64651
yun-jiyeong <70522500+jiyeong-yun@users.noreply.github.com>
2021-06-07 09:08:34 +0900

    최종 수정

--- tensorflow/tools/android/test/src/org/tensorflow/demo/activity_weather.java
+++ tensorflow/tools/android/test/src/org/tensorflow/demo/activity_weather.java
@@ -379,11 +379,15 @@ public class activity_weather extends Activity {
                     case 200: case 201: case 202: case 210: case 211: case 212: case 221: case 230: case 231: case 232:
                         tvStatus.setText("뇌우");
                         break;
+                    /*// 이슬비
+                    case 300: case 301: case 302: case 310: case 311: case 312: case 313: case 314: case 321:
+                        tvStatus.setText("이슬비");
+                        break;*/
                     //비
-                    case 500: case 501: case 502: case 503: case 504: case 511: case 520: case 521: case 522: case 531: case 300: case 301: case 302: case 310: case 311: case 312: case 313: case 314: case 321:
+                    case 300: case 301: case 302: case 310: case 311: case 312: case 313: case 314: case 321: case 500: case 501: case 502: case 503: case 504: case 511: case 520: case 521: case 522: case 531:
                         tvStatus.setText("비");
-
-                        //눈
+                        break;
+                     //눈
                     case 600: case 601: case 602: case 611: case 612: case 615: case 616: case 620: case 621: case 622:
                         tvStatus.setText("눈");
                         break;
